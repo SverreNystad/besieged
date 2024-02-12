31821c6fd444a8bb07e6d3dd68a4af828776d99a
Sverre Nystad <89105607+SverreNystad@users.noreply.github.com>
2024-02-12 01:04:26 +0100

    Build: Configure Junit tests

---
+++ core/test/com/softwarearchitecture/GameAppTest.java
@@ -0,0 +1,24 @@
+package com.softwarearchitecture;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class GameAppTest {
+
+    private GameApp gameApp;
+
+    @Before
+    public void setUp() throws Exception {
+        // This method is called before each test case.
+        // Initialize your class here.
+        gameApp = new GameApp();
+    }
+
+    @Test
+    public void testSomeFunctionality() {
+        // Here you can test a method of GameApp.
+        assertTrue(true);
+    }
+
+}
