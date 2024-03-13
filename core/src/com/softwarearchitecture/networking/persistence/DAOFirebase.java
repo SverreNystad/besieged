5134707986c6d6e81ddd375b83901f5a1e7d2614
Sverre Nystad <89105607+SverreNystad@users.noreply.github.com>
2024-03-13 01:09:45 +0100

    feat: create DAOFirebase

--- core/src/com/softwarearchitecture/networking/persistence/DAOFirebase.java
+++ core/src/com/softwarearchitecture/networking/persistence/DAOFirebase.java
@@ -1,5 +1,37 @@
 package com.softwarearchitecture.networking.persistence;
 
-public class DAOFirebase implements DAO {
+import java.util.List;
+
+public class DAOFirebase<T,K> implements DAO<T,K> {
+
+    @Override
+    public List<T> loadAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'loadAll'");
+    }
+
+    @Override
+    public T get(K id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'get'");
+    }
+
+    @Override
+    public boolean update(K id, T object) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public boolean delete(K id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    }
+
+    @Override
+    public boolean add(T object) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'add'");
+    }
 
 }
