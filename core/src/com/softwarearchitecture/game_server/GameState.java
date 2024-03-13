ac27518f75fc0a8a91492470282e4a1b477bc3b3
Tim Matras <matras.tim@gmail.com>
2024-03-13 14:19:28 +0100

    Make GameState Externalizable(Serializable)

--- core/src/com/softwarearchitecture/game_server/GameState.java
+++ core/src/com/softwarearchitecture/game_server/GameState.java
@@ -1,5 +1,23 @@
 package com.softwarearchitecture.game_server;
 
-public class GameState {
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
 
+public class GameState implements Externalizable {
+
+    @Override
+    public void writeExternal(ObjectOutput out) throws IOException {
+        // TODO Auto-generated method stub
+        
+        throw new UnsupportedOperationException("Unimplemented method 'writeExternal'");
+    }
+
+    @Override
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'readExternal'");
+    }
+    
 }
