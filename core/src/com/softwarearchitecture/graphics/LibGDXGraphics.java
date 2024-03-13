516e0adfbb86761bd4daedc29d0096bc42fc1b7c
Jens Martin Norheim Berget <jmberget@stud.ntnu.no>
2024-03-13 23:25:03 +0100

    feat: :sparkles: Add unimplemented abstract-methods in LibGDX-components
    Co-authored-by: Sverre Nystad <89105607+SverreNystad@users.noreply.github.com>

--- core/src/com/softwarearchitecture/graphics/LibGDXGraphics.java
+++ core/src/com/softwarearchitecture/graphics/LibGDXGraphics.java
@@ -1,7 +1,14 @@
 package com.softwarearchitecture.graphics;
 
+import com.softwarearchitecture.ecs.components.DrawableComponent;
 import com.softwarearchitecture.game_client.GraphicsController;
 
 public class LibGDXGraphics implements GraphicsController {
+
+    @Override
+    public void draw(DrawableComponent component) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'draw'");
+    }
 
 }
