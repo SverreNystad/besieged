4832cd09eeea6db6897e84201d8dad850831fd04
Jens Martin Norheim Berget <jmberget@stud.ntnu.no>
2024-03-13 23:23:09 +0100

    feat: :sparkles: Add boilerplate update()-method in remaining ECS-systems
    Co-authored-by: Sverre Nystad <89105607+SverreNystad@users.noreply.github.com>
    
    #10

--- core/src/com/softwarearchitecture/ECS/Systems/AudioSystem.java
+++ core/src/com/softwarearchitecture/ECS/Systems/AudioSystem.java
@@ -1,5 +1,16 @@
 package com.softwarearchitecture.ecs.systems;
 
-public class AudioSystem {
+import java.util.Set;
+
+import com.softwarearchitecture.ecs.Entity;
+import com.softwarearchitecture.ecs.System;
+
+public class AudioSystem implements System {
+
+    @Override
+    public void update(Set<Entity> entities, float deltaTime) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
     
 }
--- core/src/com/softwarearchitecture/ECS/Systems/InputSystem.java
+++ core/src/com/softwarearchitecture/ECS/Systems/InputSystem.java
@@ -1,5 +1,18 @@
 package com.softwarearchitecture.ecs.systems;
 
-public class InputSystem {
+import java.util.Set;
+
+import com.softwarearchitecture.ecs.Entity;
+import com.softwarearchitecture.ecs.System;
+
+public class InputSystem implements System {
+
+    @Override
+    public void update(Set<Entity> entities, float deltaTime) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+    
+
     
 }
