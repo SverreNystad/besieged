c0f4fc74eb485bbcddc13fca392106a8f3364ee6
Sverre Nystad <89105607+SverreNystad@users.noreply.github.com>
2024-03-14 13:32:41 +0100

    test: add test for adding only unique entities to ECSManager

--- core/test/com/softwarearchitecture/ecs/ECSManagerTest.java
+++ core/test/com/softwarearchitecture/ecs/ECSManagerTest.java
@@ -39,5 +39,16 @@ public class ECSManagerTest {
         assertTrue("The manager should contain the first entity", manager.getEntities().contains(entity1));
         assertTrue("The manager should contain the second entity", manager.getEntities().contains(entity2));
     }
+    
+    @Test
+    public void testAddingOnlyUniqueEntities() {
+        Entity entity1 = new Entity();
+        Entity entity2 = new Entity();
+        manager.addEntity(entity1);
+        manager.addEntity(entity2);
+        manager.addEntity(entity1);
+        int amount_of_entities = manager.getEntities().size();
+        assertTrue("There should be two entities in the manager", amount_of_entities == 2);
+    }
 
 }
