b4e348f7cfc0785d05bb127a74cc18b13e7d98ba
Tim Matras <matras.tim@gmail.com>
2024-04-03 15:59:42 +0200

    wip: LibGDXGraphics

--- core/src/com/softwarearchitecture/graphics/LibGDXGraphics.java
+++ core/src/com/softwarearchitecture/graphics/LibGDXGraphics.java
@@ -1,14 +1,33 @@
 package com.softwarearchitecture.graphics;
 
+import java.util.HashMap;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.softwarearchitecture.ecs.GraphicsController;
 import com.softwarearchitecture.ecs.components.SpriteComponent;
 
 public class LibGDXGraphics implements GraphicsController {
+    private SpriteBatch batch;
+	private HashMap<String, Texture> textures;
+
+    public LibGDXGraphics() {
+        batch = new SpriteBatch();
+        textures = new HashMap<>();
+    }
 
     @Override
     public void draw(SpriteComponent component) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'draw'");
+        if (!textures.containsKey(component.texture_path)) {
+            textures.put(component.texture_path, new Texture(component.texture_path));
+        }
+        Texture texture = textures.get(component.texture_path);
+        
+
+        // batch.draw(texture, component.screen_u, component.screen_v, component.u_size, component.v_size);
+
+        // // TODO Auto-generated method stub
+        // throw new UnsupportedOperationException("Unimplemented method 'draw'");
     }
 
 }
