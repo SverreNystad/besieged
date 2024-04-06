2856416d8fdb271907a989f6c193685d17fe73d5
Tobias Fremming <tobif@stud.ntnu.no>
2024-04-06 15:17:05 +0200

    feat: add some images of towers into assets

--- core/src/com/softwarearchitecture/game_client/GraphicsController.java
+++ core/src/com/softwarearchitecture/game_client/GraphicsController.java
@@ -3,5 +3,15 @@ package com.softwarearchitecture.game_client;
 import com.softwarearchitecture.ecs.components.SpriteComponent;
 
 public interface GraphicsController {
+    /**
+     * Draw the sprite component on the screen.
+     * 
+     * @param component
+     */
     void draw(SpriteComponent component);
+
+    /**
+     * Clear the screen.
+     */
+    void clearScreen();
 }
