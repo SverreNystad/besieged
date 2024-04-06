df89428900b1ccc743054b9e6b5c721957f01a85
SverreNystad <sverrenystad@gmail.com>
2024-04-06 21:02:42 +0200

    feat: Update game server interfaces and implementations and add docs

--- core/src/com/softwarearchitecture/networking/messaging/ServerMessenger.java
+++ core/src/com/softwarearchitecture/networking/messaging/ServerMessenger.java
@@ -5,7 +5,7 @@ import java.util.UUID;
 
 import com.softwarearchitecture.game_server.PlayerInput;
 import com.softwarearchitecture.game_server.ServerMessagingController;
-import com.softwarearchitecture.game_server.states.InGame;
+import com.softwarearchitecture.game_server.GameState;
 
 public class ServerMessenger implements ServerMessagingController {
 
@@ -16,7 +16,7 @@ public class ServerMessenger implements ServerMessagingController {
     }
 
     @Override
-    public void setNewGameState(InGame gameState) {
+    public void setNewGameState(GameState gameState) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'setNewGameState'");
     }
