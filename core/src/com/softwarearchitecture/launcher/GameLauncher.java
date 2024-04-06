f53fdf39ed56810773e53cedb12c88f5f66bc3e7
SverreNystad <sverrenystad@gmail.com>
2024-04-06 16:35:23 +0200

    feat: Add audio system to GameLauncher

--- core/src/com/softwarearchitecture/launcher/GameLauncher.java
+++ core/src/com/softwarearchitecture/launcher/GameLauncher.java
@@ -1,11 +1,15 @@
 package com.softwarearchitecture.launcher;
 
+import com.softwarearchitecture.ecs.ComponentManager;
+import com.softwarearchitecture.ecs.ECSManager;
 import com.softwarearchitecture.ecs.GraphicsController;
+import com.softwarearchitecture.ecs.systems.AudioSystem;
 import com.softwarearchitecture.ecs.systems.InputSystem;
 import com.softwarearchitecture.game_client.GameClient;
 import com.softwarearchitecture.graphics.LibGDXGraphics;
 import com.softwarearchitecture.input.LibGDXInput;
 import com.softwarearchitecture.ecs.SoundController;
+import com.softwarearchitecture.ecs.components.SoundComponent;
 import com.softwarearchitecture.game_client.GameClient;
 import com.softwarearchitecture.graphics.LibGDXGraphics;
 import com.softwarearchitecture.sound.LibGDXSound;
@@ -24,8 +28,12 @@ public class GameLauncher {
         libGDXInput.onRelease(touchLocation -> inputSystem.onRelease(touchLocation));
 
         // Set up audio-system
-        SoundController audioController = new LibGDXSound();
-
+        // TODO: Get volume from settings
+        float settingsVolume = 0.5f;
+        SoundController audioController = new LibGDXSound(settingsVolume);
+        ComponentManager<SoundComponent> audioManager = ECSManager.getInstance().getOrDefaultComponentManager(SoundComponent.class);
+        AudioSystem audioSystem = new AudioSystem(audioManager, audioController);
+        ECSManager.getInstance().addSystem(audioSystem);
         // Initiate client
         GameClient gameClient = new GameClient(graphicsController, inputSystem);
         gameClient.run();
