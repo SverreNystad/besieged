61180a95d7158e514956d7bd469bfd0be0c57ef3
SverreNystad <sverrenystad@gmail.com>
2024-04-06 15:23:20 +0200

    feat: Add sound support to GameLauncher

--- core/src/com/softwarearchitecture/launcher/GameLauncher.java
+++ core/src/com/softwarearchitecture/launcher/GameLauncher.java
@@ -1,8 +1,10 @@
 package com.softwarearchitecture.launcher;
 
 import com.softwarearchitecture.ecs.GraphicsController;
+import com.softwarearchitecture.ecs.SoundController;
 import com.softwarearchitecture.game_client.GameClient;
 import com.softwarearchitecture.graphics.LibGDXGraphics;
+import com.softwarearchitecture.sound.LibGDXSound;
 
 public class GameLauncher {
     /**
@@ -10,6 +12,7 @@ public class GameLauncher {
      */
     public static void startGame() {
         GraphicsController graphicsController = new LibGDXGraphics();
+        SoundController audioController = new LibGDXSound();
 		GameClient gameClient = new GameClient(graphicsController);
         gameClient.run();
     }
