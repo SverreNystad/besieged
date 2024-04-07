59996eac2c1fc17232f09c5c116415b70f36fc3d
Tobias Fremming <tobif@stud.ntnu.no>
2024-04-07 19:49:08 +0200

    feat: add tower factory class

---
+++ core/src/com/softwarearchitecture/game_server/TowerFactory.java
@@ -0,0 +1,12 @@
+package com.softwarearchitecture.game_server;
+
+import com.badlogic.gdx.math.Vector2;
+import com.softwarearchitecture.ecs.Entity;
+
+public class TowerFactory {
+
+    public static Entity createTower() {
+
+    }
+
+}
