8f4b4fd985fb23a30bbf2e413e831abfe8fee54c
Tobias Fremming <tobif@stud.ntnu.no>
2024-04-07 19:55:46 +0200

    refactor: add card enum to cardComponent in ecs

--- core/src/com/softwarearchitecture/ecs/components/PlacedCardComponent.java
+++ core/src/com/softwarearchitecture/ecs/components/PlacedCardComponent.java
@@ -2,12 +2,17 @@ package com.softwarearchitecture.ecs.components;
 
 import java.io.Serializable;
 
+import com.softwarearchitecture.game_server.CardFactory;
+import com.softwarearchitecture.game_server.CardFactory.CardType;
+
 public class PlacedCardComponent implements Serializable {
     public int x;
     public int y;
+    public CardType cardType;
 
-    public PlacedCardComponent(int x, int y) {
+    public PlacedCardComponent(int x, int y, CardType cardType) {
         this.x = x;
         this.y = y;
+        this.cardType = cardType;
     }
 }
