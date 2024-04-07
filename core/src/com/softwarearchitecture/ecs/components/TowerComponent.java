dda6284c7f740b6c56dcb2b000d89ed0ad0a2e35
Jens Martin Norheim Berget <jmberget@stud.ntnu.no>
2024-04-07 20:03:17 +0200

    feat: :sparkles: Added range-attribute in TowerComponent in ECS
    #17

--- core/src/com/softwarearchitecture/ecs/components/TowerComponent.java
+++ core/src/com/softwarearchitecture/ecs/components/TowerComponent.java
@@ -11,19 +11,26 @@ public class TowerComponent implements Serializable {
 
     public List<DamageType> damageTypes;
     private int damage;
+    private int range;
 
-    public TowerComponent(List<DamageType> damageTypes, int damage) {
+    public TowerComponent(List<DamageType> damageTypes, int damage, int range) {
         this.damageTypes = new ArrayList<>(damageTypes);
-        if (damage >= 1) {
+        if (damage >= 1 && range >= 1) {
             this.damage = damage;
+            this.range = range;
         }
         else {
             this.damage = 1;
+            this.range = 1;
         }
     }
 
     public int getDamage() {
         return this.damage;
+    }
+
+    public int getRange() {
+        return this.range;
     }
 
     public void setDamage(int damage) {
@@ -32,6 +39,15 @@ public class TowerComponent implements Serializable {
         }
         else {
             this.damage = 1;
+        }
+    }
+
+    public void setRange(int range) {
+        if (range >= 1) {
+            this.range = range;
+        }
+        else {
+            this.range = 1;
         }
     }
 }
