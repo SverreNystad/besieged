9775533f6ade85c9cad2423687596d25b7bdf3c1
Tobias Fremming <tobif@stud.ntnu.no>
2024-04-07 22:03:33 +0200

    clean: remove print statements

--- core/src/com/softwarearchitecture/launcher/GameLauncher.java
+++ core/src/com/softwarearchitecture/launcher/GameLauncher.java
@@ -23,50 +23,49 @@ import com.softwarearchitecture.graphics.LibGDXGraphics;
 import com.softwarearchitecture.sound.LibGDXSound;
 
 public class GameLauncher {
-    /**
-     * Create a new game client.
-     */
-    public static GameClient createGameClient() {
+        /**
+         * Create a new game client.
+         */
+        public static GameClient createGameClient() {
 
-        ComponentManager<ButtonComponent> buttonManager = ECSManager.getInstance()
-                .getOrDefaultComponentManager(ButtonComponent.class);
-        LibGDXInput libGDXInput = new LibGDXInput();
-        InputSystem inputSystem = new InputSystem(buttonManager, libGDXInput);
-        ECSManager.getInstance().addSystem(inputSystem);
+                ComponentManager<ButtonComponent> buttonManager = ECSManager.getInstance()
+                                .getOrDefaultComponentManager(ButtonComponent.class);
+                LibGDXInput libGDXInput = new LibGDXInput();
+                InputSystem inputSystem = new InputSystem(buttonManager, libGDXInput);
+                ECSManager.getInstance().addSystem(inputSystem);
 
-        // Set to main manu
-        ScreenManager screenManager = ScreenManager.getInstance();
-        screenManager.nextState(new Menu(MenuEnum.MENU));
+                // Set to main manu
+                ScreenManager screenManager = ScreenManager.getInstance();
+                screenManager.nextState(new Menu(MenuEnum.MENU));
 
-        // Set up input-system
-        libGDXInput.onTouch(touchLocation -> inputSystem.onTouch(touchLocation));
-        libGDXInput.onRelease(touchLocation -> inputSystem.onRelease(touchLocation));
+                // Set up input-system
+                libGDXInput.onTouch(touchLocation -> inputSystem.onTouch(touchLocation));
+                libGDXInput.onRelease(touchLocation -> inputSystem.onRelease(touchLocation));
 
-        // Set up graphics-system
-        ComponentManager<SpriteComponent> spriComponentManager = ECSManager.getInstance()
-                .getOrDefaultComponentManager(SpriteComponent.class);
-        ComponentManager<PositionComponent> positionComponentManager = ECSManager.getInstance()
-                .getOrDefaultComponentManager(PositionComponent.class);
-        GraphicsController graphicsController = new LibGDXGraphics();
-        RenderingSystem renderingSystem = new RenderingSystem(spriComponentManager, positionComponentManager,
-                graphicsController);
-        ECSManager.getInstance().addSystem(renderingSystem);
+                // Set up graphics-system
+                ComponentManager<SpriteComponent> spriComponentManager = ECSManager.getInstance()
+                                .getOrDefaultComponentManager(SpriteComponent.class);
+                ComponentManager<PositionComponent> positionComponentManager = ECSManager.getInstance()
+                                .getOrDefaultComponentManager(PositionComponent.class);
+                GraphicsController graphicsController = new LibGDXGraphics();
+                RenderingSystem renderingSystem = new RenderingSystem(spriComponentManager, positionComponentManager,
+                                graphicsController);
+                ECSManager.getInstance().addSystem(renderingSystem);
 
-        // Set up audio-system
-        // TODO: Get volume from settings
-        float settingsVolume = 0.5f;
-        SoundController audioController = new LibGDXSound(settingsVolume);
-        ComponentManager<SoundComponent> audioManager = ECSManager.getInstance()
-                .getOrDefaultComponentManager(SoundComponent.class);
-        AudioSystem audioSystem = new AudioSystem(audioManager, audioController);
-        ECSManager.getInstance().addSystem(audioSystem);
-        // Initiate client
-        GameClient gameClient = new GameClient(graphicsController, inputSystem);
-        // gameClient.run();
-        System.out.println(ECSManager.getInstance().getSystems());
-        ECSManager.getInstance().update(1);
+                // Set up audio-system
+                // TODO: Get volume from settings
+                float settingsVolume = 0.5f;
+                SoundController audioController = new LibGDXSound(settingsVolume);
+                ComponentManager<SoundComponent> audioManager = ECSManager.getInstance()
+                                .getOrDefaultComponentManager(SoundComponent.class);
+                AudioSystem audioSystem = new AudioSystem(audioManager, audioController);
+                ECSManager.getInstance().addSystem(audioSystem);
+                // Initiate client
+                GameClient gameClient = new GameClient(graphicsController, inputSystem);
+                // gameClient.run();
+                ECSManager.getInstance().update(1);
 
-        gameClient.init();
-        return gameClient;
-    }
+                gameClient.init();
+                return gameClient;
+        }
 }
