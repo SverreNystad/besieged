698e797187240232bf768ec15d51b91f2125a13f
Jens Martin Norheim Berget <jmberget@stud.ntnu.no>
2024-04-07 14:21:54 +0200

    feat: :sparkles: Change Card-interface to abstract class and create structure by using some of the ECS-components
    #17

--- core/src/com/softwarearchitecture/ecs/Card.java
+++ core/src/com/softwarearchitecture/ecs/Card.java
@@ -1,9 +1,22 @@
 package com.softwarearchitecture.ecs;
 
+import com.softwarearchitecture.ecs.components.CostComponent;
+import com.softwarearchitecture.ecs.components.PlacedCardComponent;
+import com.softwarearchitecture.ecs.components.SpriteComponent;
+
 /**
  * The {@code Card} interface serves as a marker interface for all card types
  * in the game. 
  */
-public interface Card {
+public abstract class Card {
     
+    private PlacedCardComponent placedCardComponent; // Position of the card on the board
+    private CostComponent costComponent; 
+    private SpriteComponent spriteComponent;
+
+    public Card(PlacedCardComponent placedCardComponent, CostComponent costComponent, SpriteComponent spriteComponent) {
+        this.placedCardComponent = placedCardComponent;
+        this.costComponent = costComponent;
+        this.spriteComponent = spriteComponent;
+    }
 }
