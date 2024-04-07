d985651d778e74150267a0163a858d9b44efd7bd
Tobias Fremming <tobif@stud.ntnu.no>
2024-04-07 20:46:14 +0200

    feat: Add components to and finish CardFactory

--- core/src/com/softwarearchitecture/game_server/CardFactory.java
+++ core/src/com/softwarearchitecture/game_server/CardFactory.java
@@ -3,7 +3,15 @@ package com.softwarearchitecture.game_server;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.text.Position;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.softwarearchitecture.ecs.Entity;
+import com.softwarearchitecture.ecs.components.MoneyComponent;
+import com.softwarearchitecture.ecs.components.PlacedCardComponent;
+import com.softwarearchitecture.ecs.components.PositionComponent;
+import com.softwarearchitecture.ecs.components.SoundComponent;
+import com.softwarearchitecture.ecs.components.SpriteComponent;
 import com.softwarearchitecture.math.Vector2;
 
 public class CardFactory {
@@ -63,6 +71,21 @@ public class CardFactory {
             default:
                 throw new IllegalArgumentException("Invalid card type");
         }
+
+        PlacedCardComponent placedCardComponent = new PlacedCardComponent((int) position.x, (int) position.y, type);
+        PositionComponent positionComponent = new PositionComponent(position);
+        SpriteComponent spriteComponent = new SpriteComponent(texture, size, 0);
+        SoundComponent soundComponent = new SoundComponent(sound);
+        MoneyComponent moneyComponent = new MoneyComponent(cost);
+
+        Entity cardEntity = new Entity();
+        cardEntity.addComponent(PlacedCardComponent.class, placedCardComponent);
+        cardEntity.addComponent(PositionComponent.class, positionComponent);
+        cardEntity.addComponent(SpriteComponent.class, spriteComponent);
+        cardEntity.addComponent(SoundComponent.class, soundComponent);
+        cardEntity.addComponent(MoneyComponent.class, moneyComponent);
+
+        return cardEntity;
     }
 
 }
