2754443cd5186c4eea82f74b8b31bd0b92a90e8b
Tobias Fremming <tobif@stud.ntnu.no>
2024-04-12 15:54:41 +0200

    feat: add textures

--- core/src/com/softwarearchitecture/game_server/states/TexturePack.java
+++ core/src/com/softwarearchitecture/game_server/states/TexturePack.java
@@ -16,4 +16,26 @@ public class TexturePack {
     // Buttons
     public static final Texture BUTTON_PLACEHOLDER = new Texture("button_placeholder.png");
 
+    // Towers
+    public static final Texture TOWER_TOR_FRAME1 = new Texture("tor_frame1.png");
+    public static final Texture TOWER_TOR_FRAME2 = new Texture("tor_frame2.png");
+    public static final Texture TOWER_TOR_FRAME3 = new Texture("tor_frame3.png");
+
+    public static final Texture TOWER_MAGIC_FRAME1 = new Texture("magic_tower_frame1.png");
+    public static final Texture TOWER_MAGIC_FRAME2 = new Texture("magic_tower_frame2.png");
+    public static final Texture TOWER_MAGIC_FRAME3 = new Texture("magic_tower_frame3.png");
+
+    public static final Texture TOWER_FIRE_FRAME1 = new Texture("magic_fire_frame1.png");
+    public static final Texture TOWER_FIRE_FRAME2 = new Texture("magic_fire_frame2.png");
+    public static final Texture TOWER_FIRE_FRAME3 = new Texture("magic_fire_frame3.png");
+
+    public static final Texture TOWER_FIRE_ATTACK_FRAME1 = new Texture("magic_fire_attack_frame1.png");
+    public static final Texture TOWER_FIRE_ATTACK_FRAME2 = new Texture("magic_fire_attack_frame2.png");
+    public static final Texture TOWER_FIRE_ATTACK_FRAME3 = new Texture("magic_fire_attack_frame3.png");
+
+    public static final Texture TOWER_BOW = new Texture("bow_tower.png");
+    public static final Texture TOWER_BOW_FIRE = new Texture("bow_fire.png");
+    public static final Texture TOWER_SHARPSHOOTER = new Texture("sharpshooter_tower.png");
+
+    
 }
