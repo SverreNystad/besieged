5835ba9424b8754814b548dbf9ad66d8c2e8527e
Tim Matras <matras.tim@gmail.com>
2024-04-13 18:05:46 +0200

    feat: Join as player two

--- core/src/com/softwarearchitecture/game_server/GameServer.java
+++ core/src/com/softwarearchitecture/game_server/GameServer.java
@@ -1,14 +1,46 @@
 package com.softwarearchitecture.game_server;
 
-public class GameServer {
-    
-    public GameServer() {
+import java.util.Optional;
+import java.util.UUID;
 
+import com.softwarearchitecture.ecs.ComponentManager;
+import com.softwarearchitecture.ecs.ECSManager;
+import com.softwarearchitecture.ecs.Entity;
+import com.softwarearchitecture.ecs.components.PlayerComponent;
+
+public class GameServer {
+    private UUID gameId;
+    private UUID playerOneID;
+    private ServerMessagingController messageController;
+
+    public GameServer(ServerMessagingController messageController, UUID playerOneID) {
+        this.messageController = messageController;        
+        this.playerOneID = playerOneID;
     }
 
     public void run() {
+        this.gameId = messageController.createGame();
+        GameState gameState = messageController.getGameState(gameId);
+        if (messageController.getGameState(gameId).playerOne == null) {
+            gameState.playerOne = new Entity();
+            gameState.playerOne.addComponent(PlayerComponent.class, new PlayerComponent(playerOneID));
+            ECSManager.getInstance().addEntity(gameState.playerOne);
+        }
+        messageController.setNewGameState(this.gameId, gameState);
+
         while(true) {
-            // TODO: Implement game server logic
+            if (messageController.getGameState(gameId).playerTwo == null) {
+                Optional<UUID> playerTwo = messageController.lookForPendingPlayer(gameId);
+                if (playerTwo.isEmpty()) continue;
+
+                gameState.playerTwo = new Entity();
+                gameState.playerTwo.addComponent(PlayerComponent.class, new PlayerComponent(playerTwo.get()));
+                ECSManager.getInstance().addEntity(gameState.playerTwo);
+
+                messageController.setNewGameState(this.gameId, gameState);
+            } else continue;
+
+            
         }
     }
 
