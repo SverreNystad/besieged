d475390472334ddb8a7ce113841e6d3c748f2bda
Tim Matras <matras.tim@gmail.com>
2024-04-13 18:22:02 +0200

    feat: Add adding of self player component to ecs

--- core/src/com/softwarearchitecture/game_client/GameClient.java
+++ core/src/com/softwarearchitecture/game_client/GameClient.java
@@ -1,7 +1,15 @@
 package com.softwarearchitecture.game_client;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
+
+import com.softwarearchitecture.ecs.ComponentManager;
 import com.softwarearchitecture.ecs.Controllers;
 import com.softwarearchitecture.ecs.ECSManager;
+import com.softwarearchitecture.ecs.Entity;
+import com.softwarearchitecture.ecs.components.PlayerComponent;
 import com.softwarearchitecture.game_client.states.Menu;
 import com.softwarearchitecture.game_client.states.MenuEnum;
 import com.softwarearchitecture.game_client.states.ScreenManager;
@@ -10,11 +18,14 @@ public class GameClient {
     private Controllers defaultControllers;
     private ScreenManager screenManager;
     private static Process serverProcess = null;
+    private UUID yourId;
 
     public GameClient(Controllers defaultControllers) throws IllegalArgumentException {
         this.defaultControllers = defaultControllers;
         screenManager = ScreenManager.getInstance();
         screenManager.nextState(new Menu(MenuEnum.MENU, defaultControllers));
+        yourId = UUID.randomUUID();
+
     }
     
 
@@ -25,8 +36,27 @@ public class GameClient {
     public void update() {
         screenManager.activateCurrentStateIfChanged();
         
+        addSelfPlayerIfNotExists();
+
         float deltaTime = 1f; // TODO: Implement deltatime
 
         ECSManager.getInstance().update(deltaTime);
+    }
+
+    private void addSelfPlayerIfNotExists() {
+        Set<Entity> entities = ECSManager.getInstance().getEntities();
+        boolean hasPlayer = false;
+        for (Entity entity : entities) {
+            ComponentManager<PlayerComponent> playerComponentManager = ECSManager.getInstance().getOrDefaultComponentManager(PlayerComponent.class);
+            Optional<PlayerComponent> playerComponent = playerComponentManager.getComponent(entity);
+            if (!(playerComponent.isPresent() && playerComponent.get().playerID == yourId)) continue;
+            hasPlayer = true;
+            break;
+        }
+        if (!hasPlayer) {
+            Entity player = new Entity();
+            player.addComponent(PlayerComponent.class, new PlayerComponent(yourId));
+            ECSManager.getInstance().addEntity(player);
+        }
     }
 }
