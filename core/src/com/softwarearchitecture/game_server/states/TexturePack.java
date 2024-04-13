269b0174141e4b4893535a309888d27a16e1d16f
Tobias Fremming <tobif@stud.ntnu.no>
2024-04-13 21:19:40 +0200

    feat: add texture

---
+++ core/src/com/softwarearchitecture/game_server/states/TexturePack.java
@@ -0,0 +1,52 @@
+package com.softwarearchitecture.game_server.states;
+
+import com.badlogic.gdx.graphics.Texture;
+
+/**
+ * The TexturePack is a collection contains all the textures used in the game.
+ */
+public class TexturePack {
+    // Backgrounds
+    public static final Texture BACKGROUND_TOR = new Texture("background_tor.png");
+    public static final Texture BACKGROUND_VIKING_BATTLE_ICE = new Texture("background_viking_battle_ice.png");
+    public static final Texture BACKGROUND_VIKING_BATTLE_FIRE = new Texture("background_viking_battle_fire.png");
+    public static final Texture BACKGROUND_FENRIS = new Texture("background_fenris.png");
+    public static final Texture BACKGROUND_GRIFFIN = new Texture("background_griffin.png");
+
+    // Buttons
+    public static final Texture BUTTON_PLACEHOLDER = new Texture("button_placeholder.png");
+
+    // Towers
+    public static final Texture TOWER_TOR_FRAME1 = new Texture("tor_frame1.png");
+    public static final Texture TOWER_TOR_FRAME2 = new Texture("tor_frame2.png");
+    public static final Texture TOWER_TOR_FRAME3 = new Texture("tor_frame3.png");
+
+    public static final Texture TOWER_MAGIC_FRAME1 = new Texture("magic_tower_frame1.png");
+    public static final Texture TOWER_MAGIC_FRAME2 = new Texture("magic_tower_frame2.png");
+    public static final Texture TOWER_MAGIC_FRAME3 = new Texture("magic_tower_frame3.png");
+
+    public static final Texture TOWER_FIRE_FRAME1 = new Texture("magic_fire_frame1.png");
+    public static final Texture TOWER_FIRE_FRAME2 = new Texture("magic_fire_frame2.png");
+    public static final Texture TOWER_FIRE_FRAME3 = new Texture("magic_fire_frame3.png");
+
+    public static final Texture TOWER_FIRE_ATTACK_FRAME1 = new Texture("magic_fire_attack_frame1.png");
+    public static final Texture TOWER_FIRE_ATTACK_FRAME2 = new Texture("magic_fire_attack_frame2.png");
+    public static final Texture TOWER_FIRE_ATTACK_FRAME3 = new Texture("magic_fire_attack_frame3.png");
+
+    public static final Texture TOWER_BOW = new Texture("bow_tower.png");
+    public static final Texture TOWER_BOW_FIRE = new Texture("bow_fire.png");
+    public static final Texture TOWER_SHARPSHOOTER = new Texture("sharpshooter_tower.png");
+
+
+    // Enemies
+    public static final Texture ENEMY_ANT_FRAME1 = new Texture("ant_frame1.png");
+    public static final Texture ENEMY_ANT_FRAME2 = new Texture("ant_frame2.png");
+    public static final Texture ENEMY_ANT_FRAME3 = new Texture("ant_frame3.png");
+    public static final Texture ENEMY_ANT_FRAME4 = new Texture("ant_frame4.png");
+
+    public static final Texture ENEMY_DEAD_ANT__FRAME1 = new Texture("dead_ant_frame1.png");
+    public static final Texture ENEMY_DEAD_ANT__FRAME2 = new Texture("dead_ant_frame2.png");
+
+
+
+}
