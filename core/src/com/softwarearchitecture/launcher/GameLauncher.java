47a92cd5a0d24b5a96635ad96c974521505b8b6e
SverreNystad <sverrenystad@gmail.com>
2024-04-13 21:02:54 +0200

    feat: Add ClientMessagingController to GameLauncher

--- core/src/com/softwarearchitecture/launcher/GameLauncher.java
+++ core/src/com/softwarearchitecture/launcher/GameLauncher.java
@@ -3,10 +3,12 @@ package com.softwarearchitecture.launcher;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.softwarearchitecture.ecs.GraphicsController;
+import com.softwarearchitecture.game_client.ClientMessagingController;
 import com.softwarearchitecture.game_client.Controllers;
 import com.softwarearchitecture.game_client.GameClient;
 import com.softwarearchitecture.graphics.LibGDXGraphics;
 import com.softwarearchitecture.input.LibGDXInput;
+import com.softwarearchitecture.networking.messaging.ClientMessenger;
 import com.softwarearchitecture.networking.messaging.ServerMessenger;
 import com.softwarearchitecture.ecs.SoundController;
 import com.softwarearchitecture.sound.LibGDXSound;
@@ -20,10 +22,10 @@ public class GameLauncher {
         GraphicsController graphicsController = new LibGDXGraphics(camera, viewport);
         SoundController soundController = new LibGDXSound(50);
         ServerMessenger serverMessenger = new ServerMessenger();
+        ClientMessagingController clientMessaging = new ClientMessenger();
 
         // Set to main manu
-        Controllers defaultControllers = new Controllers(graphicsController, libGDXInput, soundController, serverMessenger);
-
+        Controllers defaultControllers = new Controllers(graphicsController, libGDXInput, soundController, serverMessenger, clientMessaging);
         return new GameClient(defaultControllers);
     }
 }
