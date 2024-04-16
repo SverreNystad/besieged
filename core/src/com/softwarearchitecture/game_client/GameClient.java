a2de1cb9565f4dba9e32d22dec9fa7023e026494
SverreNystad <sverrenystad@gmail.com>
2024-04-16 15:24:12 +0200

    feat: Add defaultControllers to GameClient constructor and add clients asking for server game state in case of player joined a game

--- core/src/com/softwarearchitecture/game_client/GameClient.java
+++ core/src/com/softwarearchitecture/game_client/GameClient.java
@@ -8,8 +8,10 @@ import com.softwarearchitecture.game_client.states.ScreenManager;
 
 public class GameClient {
     private ScreenManager screenManager;
+    private Controllers defaultControllers;
 
     public GameClient(Controllers defaultControllers, UUID yourId) throws IllegalArgumentException {
+        this.defaultControllers = defaultControllers;
         screenManager = ScreenManager.getInstance();
         screenManager.nextState(new Menu(defaultControllers, yourId));
 
@@ -21,5 +23,16 @@ public class GameClient {
         float deltaTime = 1f; // TODO: Implement deltatime
 
         ECSManager.getInstance().update(deltaTime);
+
+        // Check if the player is in a multiplayer game
+        UUID gameId = null;
+        if (screenManager.getGameId() != null) {
+            gameId = screenManager.getGameId();
+            defaultControllers.clientMessagingController.requestGameState(gameId);
+        }
+        if (defaultControllers.gameServer.getGameId() != null) {
+            gameId = defaultControllers.gameServer.getGameId();
+            defaultControllers.clientMessagingController.requestGameState(gameId);
+        }
     }
 }
