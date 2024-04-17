c5eb97926e39e30ff80bc3eba6850e242f2c5fd5
Sverre Nystad <89105607+SverreNystad@users.noreply.github.com>
2024-04-17 13:36:07 +0200

    test: make update tests

--- core/test/com/softwarearchitecture/ecs/ECSManagerTest.java
+++ core/test/com/softwarearchitecture/ecs/ECSManagerTest.java
@@ -21,6 +21,7 @@ public class ECSManagerTest {
         int amount_of_entities = manager.getEntities().size();
         assertTrue("There should be no entities in the manager", amount_of_entities == 0);
         manager.addEntity(entity);
+        manager.update(0);
         amount_of_entities = manager.getEntities().size();
         assertTrue("There should be one entity in the manager", amount_of_entities == 1);
 
@@ -33,7 +34,7 @@ public class ECSManagerTest {
 
         manager.addEntity(entity1);
         manager.addEntity(entity2);
-
+        manager.update(0);
         int amount_of_entities = manager.getEntities().size();
         assertTrue("There should be two entities in the manager", amount_of_entities == 2);
         assertTrue("The manager should contain the first entity", manager.getEntities().contains(entity1));
@@ -47,6 +48,7 @@ public class ECSManagerTest {
         manager.addEntity(entity1);
         manager.addEntity(entity2);
         manager.addEntity(entity1);
+        manager.update(0);
         int amount_of_entities = manager.getEntities().size();
         assertTrue("There should be two entities in the manager", amount_of_entities == 2);
     }
