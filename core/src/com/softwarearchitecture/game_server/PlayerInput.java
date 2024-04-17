c8e780777b8c1d0db37769aba7e582acf8c81bf9
Sverre Nystad <89105607+SverreNystad@users.noreply.github.com>
2024-04-17 17:01:00 +0200

    feat: make player input have two constructors one for card placement and one generic

--- core/src/com/softwarearchitecture/game_server/PlayerInput.java
+++ core/src/com/softwarearchitecture/game_server/PlayerInput.java
@@ -2,18 +2,43 @@ package com.softwarearchitecture.game_server;
 
 import java.util.UUID;
 
+import com.softwarearchitecture.game_server.CardFactory.CardType;
+
 public class PlayerInput {
-    private UUID playerID;
+    private UUID playerId;
+    private int x;
+    private int y;
+    private CardType cardType;
     private String action;
     
     // Maybe it is better with a player input builder? Or just playerID, action type as enum, and serialized data?
-    public PlayerInput(UUID playerID, String action) {
-        this.playerID = playerID;
+    public PlayerInput(UUID playerId, CardType cardType, int x, int y) {
+        this.playerId = playerId;
+        this.x = x;
+        this.y = y;
+        this.cardType = cardType;
+        this.action = "";
+    }
+
+    public PlayerInput(UUID playerId, String action) {
+        this.playerId = playerId;
         this.action = action;
     }
 
-    public UUID getPlayerID() {
-        return playerID;
+    public UUID getPlayerId() {
+        return playerId;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public CardType getCardType() {
+        return cardType;
     }
 
     public String getAction() {
