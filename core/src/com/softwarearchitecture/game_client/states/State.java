20755e66834e12961ccb7215d73f36546cd2f7e2
Sverre Nystad <89105607+SverreNystad@users.noreply.github.com>
2024-04-17 20:53:52 +0200

    refactor: remove last activate

--- core/src/com/softwarearchitecture/game_client/states/State.java
+++ core/src/com/softwarearchitecture/game_client/states/State.java
@@ -14,7 +14,6 @@ public abstract class State {
     protected List<Entity> buttons;
     protected Controllers defaultControllers;
     protected UUID yourId;
-    protected boolean hasBeenLateActivated = false;
 
     protected State(Controllers defaultControllers, UUID yourId) {
         this.defaultControllers = defaultControllers;
@@ -34,11 +33,4 @@ public abstract class State {
      * Activates the state, setting up the ECSManager and the buttons.
      */
     protected abstract void activate();
-
-    /**
-     */
-    public void lateActivate() {
-        hasBeenLateActivated = true;
-    }
- 
 }
