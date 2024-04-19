a8ce1e1ce041309f9f119fdf6c2197f94e55b146
Tim Matras <matras.tim@gmail.com>
2024-04-19 09:20:30 +0200

    cleanup: remove debug printlines in localdao

--- core/src/com/softwarearchitecture/networking/persistence/LocalDAO.java
+++ core/src/com/softwarearchitecture/networking/persistence/LocalDAO.java
@@ -8,19 +8,15 @@ public class LocalDAO<K,T> implements DAO<K,T> {
 
     private static ConcurrentHashMap<String, LocalDAO<?, ?>> instances = new ConcurrentHashMap<>();
 
-    private final String key;
-
     private ConcurrentHashMap<K, T> data;
 
     private LocalDAO(String key) {
         this.data = new ConcurrentHashMap<>();
-        this.key = key;
     }
 
     @SuppressWarnings("unchecked")
     public static synchronized <K, T> LocalDAO<K, T> getInstance(Class<K> kClass, Class<T> tClass) {
         String key = kClass.getName() + tClass.getName();
-        System.out.println("Number of instances: " + instances.size() + ". " + "Key: " + key);
         return (LocalDAO<K, T>) instances.computeIfAbsent(key, k -> new LocalDAO<>(key));
     }
 
@@ -31,18 +27,11 @@ public class LocalDAO<K,T> implements DAO<K,T> {
 
     @Override
     public Optional<T> get(K id) {
-        // System.out.println("Getting data for key: " + key + ". Size: " + data.size());
-        if (this.key == "java.lang.String[B") {
-            System.out.println("Getting data for key: " + id + ". Size: " + data.size());
-        }
         return Optional.ofNullable(data.get(id));
     }
 
     @Override
     public boolean update(K id, T object) {
-        if (this.key == "java.lang.String[B") {
-            System.out.println("Updating data for key: " + id + ". Size: " + data.size());
-        }
         return data.replace(id, object) != null;
     }
 
@@ -53,9 +42,6 @@ public class LocalDAO<K,T> implements DAO<K,T> {
 
     @Override
     public void add(K id, T object) {
-        if (this.key.equals("java.lang.String[B")) {
-            System.out.println("Adding data for key: " + id + ". Size: " + data.size());
-        }
         data.put(id, object);
     }
 }
