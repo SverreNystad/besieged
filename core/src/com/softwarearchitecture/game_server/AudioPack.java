6999ef16d65cd6b74291f5c2550e53092481932d
Tobias Fremming <tobif@stud.ntnu.no>
2024-04-19 13:33:09 +0200

    Add new enemy sounds and tower sounds

--- core/src/com/softwarearchitecture/game_server/AudioPack.java
+++ core/src/com/softwarearchitecture/game_server/AudioPack.java
@@ -5,8 +5,18 @@ public class AudioPack {
     // Enemy sounds
     public static final String JENS = "sounds/jens.mp3";
 
-    // Placing a card 
+    // Placing a card
     public static final String PLACING_CARD = "sounds/placing_card.mp3";
+
+    // Enemy sounds
+    public static final String ENEMY_ANT = JENS;
+    public static final String ENEMY_WOLF = JENS;
+    public static final String ENEMY_VIKING_SWORD_SHIELD = JENS;
+    public static final String ENEMY_VIKING_SWORD = JENS;
+    public static final String ENEMY_VIKING_AXE = JENS;
+    public static final String ENEMY_VIKING_SPEAR = JENS;
+    public static final String ENEMY_TROLL = JENS;
+    public static final String ENEMY_ICE_GIANT = JENS;
 
     // Tower sounds
     public static final String TOWER_TECHNOLOGY = PLACING_CARD;
@@ -17,11 +27,19 @@ public class AudioPack {
     public static final String TOWER_TOR = PLACING_CARD;
     public static final String TOWER_FIRE_BOW = PLACING_CARD;
     public static final String TOWER_FIRE_MAGIC = PLACING_CARD;
-    
+    public static final String TOWER_BOW_LIGHTING = PLACING_CARD;
+    public static final String TOWER_MAGIC_TECH = PLACING_CARD;
+    public static final String TOWER_MORTAR = PLACING_CARD;
+    public static final String TOWER_INFERNO = PLACING_CARD;
+    public static final String TOWER_FURNACE = PLACING_CARD;
+    public static final String TOWER_TESLA = PLACING_CARD;
+    public static final String TOWER_THUNDERBOLT = PLACING_CARD;
+    public static final String TOWER_BOW_MAGIC = PLACING_CARD;
+    public static final String TOWER_FIRE_LIGHTNING = PLACING_CARD;
+
     // Background music
     public static final String BACKGROUND_GIRL_SINGING = "sounds/valhalla_maiden.mp3";
     public static final String BACKGROUND_CELLO_SOLO = "sounds/viking_cello_solo.mp3";
     public static final String BACKGROUND_VIKING_CHOIR = "sounds/viking_choir.mp3";
-
 
 }
